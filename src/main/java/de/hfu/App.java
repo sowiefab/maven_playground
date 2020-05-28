5306f3f6fe06dd5b7251da0e05f9358f6046fdc2
Fabian So <fabian.sowieja@hs-furtwangen.de>
2020-05-28 20:01:43 +0200

    added rudimentary javadoc to App and made it an eclipse project

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -1,6 +1,12 @@
 package de.hfu;
 import java.util.Scanner;
 
+/**
+* <h1>Aus klein mach groß</h1>
+* Gibt alles was eingegeben wird, als Großbuchstaben aus
+* @author Fabian 
+* @version 28.05.2020
+*/
 public class App 
 {
     public static void main( String[] args )
@@ -9,6 +15,7 @@ public class App
         System.out.println("Enter a text: ");
         String input = reader.next(); 
         reader.close();
+        
         System.out.println(input.toUpperCase());
     }
 }
